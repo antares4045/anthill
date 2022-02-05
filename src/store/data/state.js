491751f87801dff075f423a117967b2b57595318
Anton Arinich <antares4045@yandex.ru>
2022-02-05 20:09:33 +0300

    подкрутил стартовые цифры

--- src/store/data/state.js
+++ src/store/data/state.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import Vector from "helpers/vector";
 
-const worldBorder = [new Vector(-1500, -1000), new Vector(1500, 1000)].map(point => point.div(2))
+const worldBorder = [new Vector(-1000, -1000), new Vector(1500, 1500)].map(point => point.div(2))
 
 export default {
-    agents : 500,
-    rotationBorder: 10,
-    speedLimits: [1, 5],
-    positionBufferSize: 200,
+    agents : 100,
+    rotationBorder: 30,
+    speedLimits: [4, 10],
+    positionBufferSize: 100,
     worldBorderSize: worldBorder[1].copy().sub(worldBorder[0]),
 
     worldBorder,
