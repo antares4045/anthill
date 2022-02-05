d6ba308b8342c10fc316c4192d930ec9aeb6f45d
Anton Arinich <antares4045@yandex.ru>
2022-02-05 20:12:25 +0300

    стартовый квадрат по цетру

--- src/store/data/state.js
+++ src/store/data/state.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Vector from "helpers/vector";
 
-const worldBorder = [new Vector(-1000, -1000), new Vector(1500, 1500)].map(point => point.div(2))
+const worldBorder = [new Vector(-1500, -1500), new Vector(1500, 1500)].map(point => point.div(2))
 
 export default {
     agents : 100,
